4228625f0f5c13529c7e7e83d1a1c324fe86e6d3
denisdelaki <denisdelaki@gmail.com>
2022-07-31 17:42:24 +0300

    fetched the cakes from the database

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Cart from "./Cart";
 import Classes from "./Classes";
  import "../App.css";
@@ -7,12 +7,17 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 
 function App() {
- 
+  const [cakes, setCakes]=useState([])
+  useEffect(() => {
+    fetch("http://localhost:8004/cakes")
+      .then(res => res.json())
+    .then(cakes=>setCakes(cakes))
+ }, [])
   return (
     <>
       <Header />
       <Routes>
-        <Route exact path="/home" element={<Home />}></Route>
+        <Route exact path="/home" element={<Home cakes={cakes} />}></Route>
         <Route exact path="/classes" element={<Classes />}></Route>
         <Route exact path="/cart" element={<Cart />}></Route>
        
