ee8d698e8b35997fcfeb22beccd1e2555ef81d69
denisdelaki <denisdelaki@gmail.com>
2022-09-05 22:21:54 +0300

    added the cart length on adding the carts to the shopping cart

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,17 @@
 import React from "react";
  import "../App.css";
 import { NavLink } from "react-router-dom";
-function Nav() {
-    return (
-        <nav>
-          
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/custom">Custom Orders</NavLink>
-            <NavLink to="/cart">Cart <span>0</span></NavLink>
-       </nav>
-    )
+function Nav({cart}) {
+    let carts=cart.length
+  return (
+    <nav>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/custom">Custom Orders</NavLink>
+      <NavLink to="/cart">
+        InCart <span>{carts}</span>
+      </NavLink>
+    </nav>
+  );
 }
 export default Nav
 
