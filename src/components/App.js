bfef738347f621b0b488b39d566639d804a42b65
denisdelaki <denisdelaki@gmail.com>
2022-09-06 22:17:24 +0300

    created a fetch request to post the the data to the cart database on click

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,28 @@ import Header from "./Header";
 
 function App() {
   const [cakes, setCakes] = useState([])
-  const [cart, setCart]=useState([])
+  const [cart, setCart] = useState([])
   useEffect(() => {
     fetch("http://localhost:8004/cakes")
       .then(res => res.json())
       .then(cakes => setCakes(cakes))
     
   }, [])
-  
+ 
   function onAddCart(cake) {
     if (cart.indexOf(cake) !== -1) return;
-    const mycart=[...cart, cake]
-    setCart(mycart)
+    
+    fetch("http://localhost:8004/cart", {
+      method: "POST",
+      headers: {
+         "Content-Type":"application/json",
+      },
+      body:JSON.stringify(cake)
+    })
+      .then((res) => res.json())
+    .then(()=>setCart(...cart, cake))
+   
+    // setCart(mycart)
     alert("Cart Added Successfully")
   }
   
@@ -37,7 +47,8 @@ function App() {
           path="/home"
           element={<Home cakes={cakes} onAddCart={onAddCart} />}
         ></Route>
-        <Route exact path="/custom" element={<Custom />}></Route>
+        <Route exact path="/custom" element={
+ <Custom /> }></Route>
         <Route
           exact
           path="/cart"
