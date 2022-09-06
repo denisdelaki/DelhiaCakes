2e76d48cf9bfc0878aac9c77a98aa8fafc94a1c1
denisdelaki <denisdelaki@gmail.com>
2022-09-06 11:49:07 +0300

    created a likes button and patch request to update the number of likes on click

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,21 +1,42 @@
 import React from "react";
 function Product({ cake, onAddCart }) {
-  
+
+  const {id}=cake
+  function handleClicks() {
+   let updatedlikes = {
+      likes: cake.likes + 1,
+   };
+    // window.location.reload()
+    fetch(` http://localhost:8004/cakes/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify(updatedlikes),
+    })
+      .then(res => res.json())
+      .then((updatedlikes))
+  }
     return (
       <div id="cakes">
         <div key={cake.id} className="cakecard">
           <img src={cake.image} alt={cake.name} height="70%" width="100%" />
-          <div className="content" >
+          <div className="content">
             <div>
               <h4>Name: {cake.name}</h4>
-              <h4>Price: {cake.price}</h4>
+              <h4>Price: Ksh. {cake.price}</h4>
             </div>
             <div>
               <h4>QNT: {cake.quantity}</h4>
               <h4>Available: {cake.available}</h4>
             </div>
           </div>
-          <button className="cartbutton" onClick={()=>onAddCart(cake)}>Add To Cart</button>
+          <button className="btn-likes" onClick={handleClicks}>
+            Likes:{cake.likes}💛
+          </button>
+          <button className="cartbutton" onClick={() => onAddCart(cake)}>
+            Add To Cart
+          </button>
         </div>
       </div>
     );
